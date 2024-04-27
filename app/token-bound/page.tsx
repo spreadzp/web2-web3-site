0e407d8815b5796f118b5eb040128ac5b4979875
Paul K <pavlo.k@blaize.tech>
2024-04-27 23:03:09 +0300

    add comment

--- app/token-bound/page.tsx
+++ app/token-bound/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import TBA from "../../components/tba";
-import Tbd from "../../components/tbd";
+// import Tbd from "../../components/tbd";
 import { useTbaSiteStore } from "../../hooks/store";
 import { WalletInstallation } from "../../hooks/walletInstallation";
 
